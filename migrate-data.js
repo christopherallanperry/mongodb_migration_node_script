f7df253518fe10905457b9f97ffff5d83d32d1e4
chrisofarabia <chrisofarabia@yahoo.co.uk>
2018-01-12 18:16:12 +0000

    Removed unnecessary node_modules and tidied up main JS file

--- migrate-data.js
+++ migrate-data.js
@@ -5,10 +5,12 @@ const customerAddresses = require('./data/customer-address-data.json');
 const async = require('async');
 
 let tasks = [];
+
 const limit = parseInt(process.argv[2], 10) || 1000;
 
 mongodb.MongoClient.connect(url, (error, database) => {
   if (error) return process.exit(1);
+
   console.log('Connection is okay');
 
   const db = database.db('edx-course-db');
@@ -30,13 +32,14 @@ mongodb.MongoClient.connect(url, (error, database) => {
   })
 
   console.log(`Launching ${tasks.length} parallel task(s)`);
+
   const startTime = Date.now();
-  
+
   async.parallel(tasks, (error, results) => {
     if (error) console.error(error);
     const endTime = Date.now();
     console.log(`Execution time: ${endTime-startTime}`);
-    // console.log(results)
+
     database.close();
   })
 })
